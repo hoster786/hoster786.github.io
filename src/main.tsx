e6bb2f0498f61dd02421267e00730d08f17ab28c
Munawar Johar <106137102+MunawarJohar@users.noreply.github.com>
2025-06-12 12:32:41 +0500

    update the packages

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
-// Use HashRouter for GitHub Pages to avoid 404 errors
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
