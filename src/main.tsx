1a54bcd947a55fc7404ee0d0c1fcb9f890e3aa56
Munawar Johar <106137102+MunawarJohar@users.noreply.github.com>
2025-06-12 12:22:06 +0500

    updated source code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import './index.css';
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+// Use HashRouter for GitHub Pages to avoid 404 errors
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/fancybook">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
